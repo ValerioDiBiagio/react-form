42f4adec8ed60a202df95c29d780d90a67eeb413
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-09 15:37:36 +0200

    feat: create articles array

---
+++ src/data/videogames.js
@@ -0,0 +1,28 @@
+const videogames =
+    [
+        {
+            nome: "The Legend of Zelda: Tears of the Kingdom",
+            piattaforma: "Nintendo Switch",
+            genere: ["Action-adventure", "Open world"]
+        },
+        {
+            nome: "Baldur's Gate 3",
+            piattaforma: ["PC", "PlayStation 5", "Xbox Series X/S"],
+            genere: ["RPG", "Strategia a turni"]
+        },
+        {
+            nome: "Red Dead Redemption 2",
+            piattaforma: ["PC", "PlayStation 4", "Xbox One"],
+            genere: ["Action-adventure", "Open world"]
+        },
+        {
+            nome: "Elden Ring",
+            piattaforma: ["PC", "PlayStation 5", "Xbox Series X/S", "PlayStation 4", "Xbox One"],
+            genere: ["Action RPG", "Souls-like", "Open world"]
+        },
+        {
+            nome: "Marvel's Spider-Man: Miles Morales",
+            piattaforma: ["PlayStation 5", "PlayStation 4", "PC"],
+            genere: ["Action-adventure", "Supereroi"]
+        }
+    ]
