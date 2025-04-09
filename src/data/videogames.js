d097985623c36579874e462d76f99be52a33aa7b
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-09 15:39:38 +0200

    feat: add id

--- src/data/videogames.js
+++ src/data/videogames.js
@@ -1,26 +1,31 @@
 const videogames =
     [
         {
+            id: 1,
             nome: "The Legend of Zelda: Tears of the Kingdom",
             piattaforma: "Nintendo Switch",
             genere: ["Action-adventure", "Open world"]
         },
         {
+            id: 2,
             nome: "Baldur's Gate 3",
             piattaforma: ["PC", "PlayStation 5", "Xbox Series X/S"],
             genere: ["RPG", "Strategia a turni"]
         },
         {
+            id: 3,
             nome: "Red Dead Redemption 2",
             piattaforma: ["PC", "PlayStation 4", "Xbox One"],
             genere: ["Action-adventure", "Open world"]
         },
         {
+            id: 4,
             nome: "Elden Ring",
             piattaforma: ["PC", "PlayStation 5", "Xbox Series X/S", "PlayStation 4", "Xbox One"],
             genere: ["Action RPG", "Souls-like", "Open world"]
         },
         {
+            id: 5,
             nome: "Marvel's Spider-Man: Miles Morales",
             piattaforma: ["PlayStation 5", "PlayStation 4", "PC"],
             genere: ["Action-adventure", "Supereroi"]
