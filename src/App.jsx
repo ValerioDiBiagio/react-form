f1a4e563164f9e8d1884d6cb4fa99a7640560a8d
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-04-09 16:18:19 +0200

    feat: add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 // importare array
 import videogames from './data/videogames';
+// importare useState
 import { useState } from 'react'
 
 function App() {
 
+  const [videogameName, setVideogameName] = useState('')
+
 
   return (
     <>
+
       <ul>
-        {videogames.map(videogame => <li>{videogame.nome}</li>)}
+        {videogames.map(videogame => <li key={videogames.id}>{videogame.nome}</li>)}
       </ul>
+      <form>
+        <input type="text" value={videogameName} onChange={e => { setVideogameName(e.target.value) }} />
+      </form>
 
     </>
   )
